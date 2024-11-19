179022605cbf8f18a6f1a4eb97e5771765870680
CinniaB <unicornbidaux@gmail.com>
2024-11-19 11:57:56 -0600

    wewgw

--- ballons/sketch.js
+++ ballons/sketch.js
@@ -12,25 +12,31 @@ function setup() {
   background(255);
 }
 function draw() {
+  background(155)
   drawTree(width/2, height*0.9, 90, 6);
   
 }
 function drawLine( x1, y1, x2, y2, depth) {
 //draw a line segment connecting (x1,y1) to (x2,y2)
+  // eslint-disable-next-line no-cond-assign
+  
   line(x1, y1, x2, y2);
+  
 }
 function drawTree(x1, y1, angle, depth) {
+  
   if (depth > 0) {
-    let angle = map(mouseX,0,width,1.01,2);
+    strokeWeight(depth*1);
+    let den = map(mouseX,0,width,1,50);
     let x2 = x1 + (cos(radians(angle))*depth*scale); //calculate endpoints ofcurrent branch
     let y2 = y1 - (sin(radians(angle))*depth*scale); //using trig ratios. Getshorter based on depth
     drawLine(x1, y1, x2, y2, depth);
     //for a 2-branch tree:
-    drawTree(x2, y2, angle, depth-1);
-    drawTree(x2, y2, angle, depth-1);
+    drawTree(x2, y2, angle+den, depth-1);
+    drawTree(x2, y2, angle-den, depth-1);
     drawTree(x2, y2, angle, depth-1);
     if(depth < 5){
-      drawLeaf(x2, y2, depth*10);
+      drawLeaf(x2, y2, depth*(random(15)));
     }
 
   }
@@ -41,13 +47,14 @@ function drawLeaf(x,y,d){
   if(d > 0){
     fill(random(255),random(255),random(255));
     circle(x,y,d);
+    strokeWeight(2);
     
     
 
 
   }
 }
-//function circleIncircle(x, y, d){
+function circleIncircle(x, y, d){
   if (d > 10){
     circle(x,y,d);
     let den = map(mouseX,0,width,1.01, 2);
